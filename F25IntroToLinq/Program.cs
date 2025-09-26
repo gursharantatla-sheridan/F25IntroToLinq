2de61da434411a37d1ccc9f873b37d359064c1fe
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-26 11:17:06 -0400

    linq ex with class objects

--- F25IntroToLinq/Program.cs
+++ F25IntroToLinq/Program.cs
@@ -47,6 +47,60 @@
             foreach (var i in startsWithR)
                 Console.WriteLine(i);
             Console.WriteLine("\n\n");
+
+
+
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee("John", "Green", 5000),
+                new Employee("Anne", "Blue", 6000),
+                new Employee("Mark", "Indigo", 4000),
+                new Employee("Mary", "White", 5500),
+                new Employee("Alice", "Indigo", 7000),
+                new Employee("Bob", "Brown", 3000)
+            };
+
+            foreach (var emp in employees)
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+
+            var between4k6k = from e in employees
+                              where e.Salary >= 4000 && e.Salary <= 6000
+                              select e;
+
+            foreach (var emp in between4k6k)
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+
+            var sortedEmps = from e in employees
+                             orderby e.LastName, e.FirstName
+                             select e;
+
+            foreach (var emp in sortedEmps)
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+
+            var lastnames = from e in employees
+                            select e.LastName;
+
+            foreach (var emp in lastnames.Distinct())
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+
+            var empNames = from e in employees
+                           select new { e.FirstName, e.LastName };
+
+            foreach (var emp in empNames)
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
         }
     }
 }
